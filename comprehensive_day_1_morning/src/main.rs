150dc040d3efa3873f27a0384e09010531c3847b
Andrea Morabito <andrea.morabito@pagopa.it>
2023-04-19 23:31:07 +0200

    add day 1 morning exercise

---
+++ comprehensive_day_1_morning/src/main.rs
@@ -0,0 +1,51 @@
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut matrix_transposed = [[0; 3]; 3];
+    for i in 0..matrix.len() {
+        for j in 0..matrix.len() {
+            matrix_transposed[i][j] = matrix[j][i];
+        }
+    }
+    // ownership transferred to caller
+    matrix_transposed
+}
+
+
+fn pretty_print(matrix: &[[i32; 3]; 3]) {
+    for row in matrix {
+        // printable only in debug mode :?
+        println!("{row:?}");
+    }
+}
+
+#[test]
+fn test_transpose() {
+    let matrix = [
+        [101, 102, 103],
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+    let transposed = transpose(matrix);
+    assert_eq!(
+        transposed,
+        [
+            [101, 201, 301],
+            [102, 202, 302],
+            [103, 203, 303],
+        ]
+    );
+}
+
+fn main() {
+    let matrix = [
+        [101, 102, 103],
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matrix:");
+    pretty_print(&matrix);
+
+    let transposed = transpose(matrix);
+    println!("transposed:");
+    pretty_print(&transposed);
+}
